aa4b0b6202ba6d27867dbd39bcd9a5f7fbe2288b
Jtboy5050 <jandrews8@cub.uca.edu>
2025-10-23 12:48:45 -0500

    Enhance game flow with opponent selection and replay
    Added option for player to choose opponent type and play again.

--- Client 1.py
+++ Client 1.py
@@ -6,45 +6,72 @@
 # Class: Networking, Honor's Contract
 # Purpose: teach the understanding of how networking works via real implementation
 
-
-# Import the network function
+# Imports the needed libraries
 from socket import *
+import os
+import time
 
-# Variable Declaration for the socket
+# Sets the server host and port to connect to
 serverHost = 'localhost'
 serverPort = 12036
 
-# Connects to the socket
-
+# Creates the socket and connects to the server
 client1 = socket(AF_INET, SOCK_STREAM)
+client1.connect((serverHost, serverPort))
 
-client1.connect(serverHost, serverPort)
+print("Sucessfully connected to the server!")
 
-print ("Sucessfully connected to the server!")
+# Asks if the player wants to play against a human or bot
+client1Message = client1.recv(1024).decode()
+print(client1Message, end='')
 
-# Checks if the connection exists. If it doesn't, disconnects the user. Otherwise, prompts the user for their message
+# Checks to see what the opponent type is, then sends it to the server
+mode = input("Choose opponent (human/bot): ").strip().lower()
+client1.send(mode.encode())
+
+# Main loop to keep the client running and responding to server messages
 while True:
-
     client1Message = client1.recv(1024).decode()
-
-    print(client1Message, end='')
     
-# The error handler
-   
+    # If there isn't a message from the server, the program assumes a disconnection and ends the game
     if not client1Message:
-
         print("Server Disconnected :(")
         break
-    
-    if ("Enter Move" in client1Message):
-        
-        move = input("Your move (rock, paper, scisossors, or quit)")
-        
+
+        # Prints whatever message the server sent
+    print(client1Message, end='')
+
+    # The check for if the server sent to enter a move
+    if "Enter your move" in client1Message:
+        move = input("Your move (rock, paper, scissors, or quit): ").strip().lower()
+
+        # Sends the move to the server
         client1.send(move.encode())
 
-    if move == "quit":
+        # If the move is quit, ends the game
+        if move == "quit":
+            print("Input is Quit. Exiting Game. Goodbye!")
+            break
 
-        print("Input is Quit. Exiting Game. Goodbye!")
-        break
+    # The check for if the server sent to ask if the player wants to play again
+        elif "Would you like to play again?" in client1Message:
 
-client1.close()
+        # Takes input from the player on if they want to play again
+         play_again = input("Would you like to play again? (yes/no): ").strip().lower()
+
+        # Sends the player's decision to the server
+        client1.send(play_again.encode())
+
+        # Pause for 3 seconds before clearing the console
+        time.sleep(3000)
+
+        # Clears the console for a fresh start if the player wants to play again
+        if play_again == "yes":
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        # If the player does not want to play again, exits the game
+        if play_again != "yes":
+            print("Exiting Game. Goodbye!")
+            break
+    # Closes the socket as a means of cleanup
+    client1.close()
